55fb062bdc610df538a5df2b31b40a30f7aa4e13
ssentamuabel <ssentamuabel90@gmail.com>
2024-03-28 08:49:54 +0300

    More work

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -4,12 +4,23 @@ import React, {Component} from 'react'
 class RefsDemo extends Component{
     constructor(props){
         super(props)
+        this.inputRef = React.createRef()
+    }
+
+    componentDidMount(){
+        this.inputRef.current.focus()
+        console.log(this.inputRef)
+    }
+
+    clickHandler = ()=>{
+        alert(this.inputRef.current.value)
     }
 
     render(){
         return(
             <div>
-                <input type="text"/>
+                <input type="text" ref={this.inputRef}/>
+                <button type="submit" onClick={this.clickHandler}>Submit</button>
             </div>
         )
     }
